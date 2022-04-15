e2fd90ef39c62b9fae4a8b294f14e872533c4e45
Lam Son Ho <holamson2000@gmail.com>
2022-04-15 08:47:14 +0700

    example 5 using orjson for response

--- example4.py
+++ example4.py
@@ -2,6 +2,9 @@ from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from datetime import datetime
+import orjson
+
 
 LOG_FILE = "./log4.txt"
 
@@ -14,7 +17,7 @@ def root(request: Request):
         f.write(repr(request.query_params))
         f.write("\n------------------")
 
-    return JSONResponse({"hello": "world", "x": 123123})
+    return JSONResponse({"hello": "world", "x": 123123, "now": datetime.now()})
 
 
 app = Starlette(
---
+++ example5.py
@@ -0,0 +1,57 @@
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import Response, BackgroundTask
+from starlette.routing import Route
+from datetime import datetime
+import numpy as np
+from typing import Any
+import orjson
+
+
+LOG_FILE = "./log4.txt"
+
+OPT = orjson.OPT_NAIVE_UTC | orjson.OPT_SERIALIZE_NUMPY
+
+
+class MyJSONResponse(Response):
+    media_type = "application/json"
+
+    def __init__(
+        self,
+        content: Any,
+        status_code: int = 200,
+        headers: dict = None,
+        media_type: str = None,
+        background: BackgroundTask = None,
+    ) -> None:
+        super().__init__(content, status_code, headers, media_type, background)
+
+    def render(self, content: Any) -> bytes:
+        return orjson.dumps(content, option=OPT)
+
+
+def root(request: Request):
+    with open(LOG_FILE, "a") as f:
+        f.write("\n------------------")
+        f.write(repr(request.headers))
+        f.write("\n------------------")
+        f.write(repr(request.query_params))
+        f.write("\n------------------")
+
+    return MyJSONResponse(
+        {
+            "hello": "world",
+            "x": np.int32(123123),
+            "now": datetime.now(),
+            "today": datetime.now().date(),
+            "arr": np.array([1.3, 5.6, 3.3], dtype='float32'),
+        }
+    )
+
+
+app = Starlette(
+    debug=True,
+    routes=[
+        Route("/", root),
+    ],
+)
